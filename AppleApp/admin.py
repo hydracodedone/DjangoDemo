6da9e0525665580882f6948b4c65f379868864ef
Hydracode <474389648@qq.com>
2021-08-08 03:29:30 +0800

    FIRST COMMIT

--- AppleApp/admin.py
+++ AppleApp/admin.py
@@ -1,2 +1,136 @@
-from django.contrib import admin
+# encoding:UTF-8
 
+from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
+from django.db.models import FloatField, Sum
+from django.utils.safestring import mark_safe
+
+from AppleApp.models import LoginUser, OwnerType, Owner, AppleType, AppleLevel, AppleMaturity, ApplePesticideResidue, \
+    ApplePackingType, AppleInstance
+
+
+class AdminAbstract(ModelAdmin):
+    actions_selection_counter = True
+    date_hierarchy = "create_time"
+    empty_value_display = "-empty-"
+    list_max_show_all = 100
+    preserve_filters = False
+    list_select_related = True
+    show_full_result_count = False
+
+
+@admin.register(LoginUser)
+class AdminLoginUser(AdminAbstract):
+    list_display = ["name", "phone_number", "login_name", "login_password"]
+    list_editable = ["name", "phone_number", "login_name"]
+    search_fields = ["name", "phone_number"]
+    list_display_links = None
+
+
+@admin.register(OwnerType)
+class AdminOwnerType(AdminAbstract):
+    pass
+
+
+@admin.register(Owner)
+class AdminOwner(AdminAbstract):
+    list_filter = ["owner_type"]
+    search_fields = ["user__uid", "user__name", "user__phone_number"]
+    list_select_related = ["user"]
+
+    def get_apple_sum_remaining(self, obj):
+        return obj.appleinstance_set.all().aggregate(
+            Sum(
+                "sum_remaining",
+                output_field=FloatField()
+            )
+        ).get("sum_remaining__sum")
+
+    def get_apple_avaliable_remaining(self, obj):
+        return obj.appleinstance_set.filter(is_available=True).all().aggregate(
+            Sum(
+                "sum_remaining",
+                output_field=FloatField()
+            )
+        ).get("sum_remaining__sum")
+
+    def record_button(self, obj):
+        button = "<a class ='icon fa fa-detail' style='color: violet' " \
+                 "href='/admin/AppleApp/appleinstance/?q={}'>detail</a>".format(obj.user.uid)
+
+        return mark_safe(button)
+
+    def get_name(self, obj):
+        return obj.user.name
+
+    get_name.short_description = "name"
+    record_button.short_description = "detail"
+    get_apple_avaliable_remaining.short_description = "avaliable_remaining"
+    get_apple_sum_remaining.short_description = "sum_remaining"
+
+    record_button.allow_tags = True
+
+    list_display = ["get_name", "get_apple_sum_remaining", "get_apple_avaliable_remaining", "record_button"]
+
+
+@admin.register(AppleType)
+class AdminAppleType(AdminAbstract):
+    pass
+
+
+@admin.register(AppleLevel)
+class AdminAppleLevel(AdminAbstract):
+    pass
+
+
+@admin.register(AppleMaturity)
+class AdminAppleMaturity(AdminAbstract):
+    pass
+
+
+@admin.register(ApplePesticideResidue)
+class AdminApplePesticideResidue(AdminAbstract):
+    pass
+
+
+@admin.register(ApplePackingType)
+class AdminApplePackingType(AdminAbstract):
+    pass
+
+
+@admin.register(AppleInstance)
+class AdminAppleInstance(AdminAbstract):
+
+    def get_owner_type(self, obj):
+        return obj.owner.owner_type.owner_name
+
+    def get_user_phone(self, obj):
+        return obj.owner.user.phone_number
+
+    def get_user_name(self, obj):
+        button = "<a class ='icon fa fa-detail' style='color: violet' " \
+                 "href='/admin/AppleApp/owner/?q={}'>{}</a>".format(obj.owner.user.uid, obj.owner.user.name)
+        return mark_safe(button)
+
+    def get_available(self, obj):
+        return obj.is_available
+
+    get_owner_type.short_description = "owner_type"
+    get_user_phone.short_description = "owner_phone"
+    get_available.short_description = "is_available"
+    get_user_name.short_description = "owner"
+
+    get_available.boolean = True
+    get_user_name.allow_tags = True
+
+    list_filter = ["type", "level", "maturity", "pesticide_residue", "packing_type", "owner__owner_type", "price",
+                   "product_time", "is_available"]
+
+    owner_info = ["get_owner_type", "get_user_name", "get_user_phone"]
+
+    raw_info = ["type", "level", "maturity", "pesticide_residue", "packing_type", "price", "sum_remaining",
+                "product_time", "get_available"]
+
+    list_display = raw_info + owner_info
+
+    search_fields = ["owner__uid", "owner__user__name"]
--- AppleApp/models.py
+++ AppleApp/models.py
@@ -1,2 +1,193 @@
-from django.db import models
+# encoding:UTF-8
 
+import datetime
+import uuid
+from datetime import datetime
+
+from django.contrib.auth.hashers import make_password
+from django.db import models
+from django.utils.timezone import utc
+
+
+def uuid_general():
+    return uuid.uuid1().hex
+
+
+def get_now_time():
+    return datetime.utcnow().replace(tzinfo=utc)
+
+
+class CommonManager(models.Manager):
+    def get_queryset(self):
+        return super(CommonManager, self).get_queryset().filter(is_deleted=False)
+
+
+class CommonData(models.Model):
+    uid = models.UUIDField(primary_key=True, auto_created=True, db_index=True, default=uuid_general)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    delete_time = models.DateTimeField(null=True, blank=True)
+    is_deleted = models.BooleanField(null=False, default=False)
+    objects = CommonManager()
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.delete_time = get_now_time()
+        self.save()
+
+
+class LoginUser(CommonData):
+    name = models.CharField(null=False, max_length=10, db_index=True)
+    phone_number = models.CharField(null=False, max_length=15)
+    login_name = models.CharField(null=False, max_length=20)
+    login_password = models.CharField(null=False, max_length=200)
+
+    def save(self, *args, **kwargs):
+        self.login_password = make_password(self.login_password)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.login_name
+
+    class Meta:
+        db_table = "Login_User"
+        unique_together = ["is_deleted", "login_name"]
+
+
+class OwnerType(CommonData):
+    owner_name = models.CharField(null=False, blank=False, unique=True, max_length=20)
+
+    def __str__(self):
+        return self.owner_name
+
+    class Meta:
+        db_table = "Owner_Type"
+
+
+class Owner(CommonData):
+    owner_type = models.ForeignKey(OwnerType, on_delete=models.CASCADE)
+    user = models.OneToOneField(LoginUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "[{}]:Name:{}".format(self.owner_type.owner_name, self.user.name)
+
+    class Meta:
+        db_table = "Owner"
+
+
+class StoragePool(CommonData):
+    longtitude = models.FloatField(null=False)
+    latitude = models.FloatField(null=False)
+    name = models.CharField(null=False, blank=False, max_length=200)
+
+    class Meta:
+        db_table = "Storage_Pool"
+
+
+class AppleType(CommonData):
+    type_name = models.CharField(null=False, blank=False, unique=True, max_length=20)
+
+    def __str__(self):
+        return self.type_name
+
+    class Meta:
+        db_table = "Apple_Type"
+
+
+class AppleLevel(CommonData):
+    level_name = models.CharField(null=False, blank=False, unique=True, max_length=20)
+
+    def __str__(self):
+        return self.level_name
+
+    class Meta:
+        db_table = "Apple_Level"
+
+
+class AppleMaturity(CommonData):
+    maturity_name = models.CharField(null=False, blank=False, unique=True, max_length=20)
+
+    def __str__(self):
+        return self.maturity_name
+
+    class Meta:
+        db_table = "Apple_Maturity"
+
+
+class ApplePesticideResidue(CommonData):
+    pesticide_residue_name = models.CharField(null=False, blank=False, unique=True, max_length=20)
+
+    def __str__(self):
+        return self.pesticide_residue_name
+
+    class Meta:
+        db_table = "Apple_Pesticide_Residue"
+
+
+class ApplePackingType(CommonData):
+    packing_type_name = models.CharField(null=False, blank=False, unique=True, max_length=20)
+
+    def __str__(self):
+        return self.packing_type_name
+
+    class Meta:
+        db_table = "Apple_Packing_Type"
+
+
+class AppleInstance(CommonData):
+    type = models.ForeignKey(AppleType, related_name="apple", on_delete=models.CASCADE)
+    level = models.ForeignKey(AppleLevel, related_name="apple", on_delete=models.CASCADE)
+    maturity = models.ForeignKey(AppleMaturity, related_name="apple", on_delete=models.CASCADE)
+    pesticide_residue = models.ForeignKey(ApplePesticideResidue, related_name="apple", on_delete=models.CASCADE)
+    packing_type = models.ForeignKey(ApplePackingType, related_name="apple", on_delete=models.CASCADE)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+
+    storage = models.ManyToManyField(StoragePool, through='AppleInstanceThroughStorage')
+
+    sum_remaining = models.FloatField(null=False)
+    price = models.FloatField(null=True)
+    product_time = models.DateField(null=False)
+    is_available = models.BooleanField(null=False, default=True)
+
+    def __str__(self):
+        return "[{}]-[{}]-[{}]-[{}]-[{}]".format(
+            self.type.type_name,
+            self.level.level_name,
+            self.maturity.maturity_name,
+            self.sum_remaining,
+            self.price
+        )
+
+    class Meta:
+        db_table = "Apple_Instance"
+
+
+class AppleInstanceThroughStorage(CommonData):
+    storage_name = models.ForeignKey(StoragePool, on_delete=models.CASCADE)
+    apple_instance = models.ForeignKey(AppleInstance, on_delete=models.CASCADE)
+    remaining = models.FloatField(null=False)
+    incoming_time = models.DateField()
+
+    class Meta:
+        db_table = "Apple_Instance_Through_Storage"
+
+
+class StoragePoolQuantityChangeLog(CommonData):
+    instance = models.ForeignKey(AppleInstanceThroughStorage, on_delete=models.CASCADE)
+    change_number = models.FloatField(null=False)
+    remaining = models.FloatField(null=False)
+
+    class Meta:
+        db_table = "Storage_Pool_Quantity_ChangeLog"
+
+
+class AppleQuantityChangeLog(CommonData):
+    instance = models.ForeignKey(AppleInstance, on_delete=models.CASCADE)
+    change_number = models.FloatField(null=False)
+    remaining = models.FloatField(null=False)
+
+    class Meta:
+        db_table = "Apple_Quantity_ChangeLog"
